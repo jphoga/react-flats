0d7736d47b30a54716b619f538b37ebf0f6b65f1
Jan Hoga <janpaulhoga@gmail.com>
2019-08-11 01:01:14 +0900

    added app component with card

---
+++ src/components/app.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="card">
+        <div className="card-category">150 EUR</div>
+        <div className="card-description">
+          <h2>Super 60m2 in trendy neighborhood!</h2>
+        </div>
+        <a className="card-link" href="#"></a>
+      </div>
+    );
+  }
+}
+
+export default App;
